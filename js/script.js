d279faa330e7143f880634d4b7c53ace0163278c
Ramires Lauer <ramireslauer96@hotmail.com>
2022-12-19 17:02:51 -0300

    Add fixed nav bar

--- js/script.js
+++ js/script.js
@@ -40,4 +40,24 @@ allLinks.forEach(function (link) {
   });
 });
 
+// fixed nav-bar
+const sectionHeroEl = document.querySelector(".section-hero");
 
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(sectionHeroEl);
